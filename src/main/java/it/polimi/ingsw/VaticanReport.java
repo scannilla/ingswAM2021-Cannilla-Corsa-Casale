4ca7621c94c764fbbc731be1884e57cbd0656b1a
Leliocasale <10582124@polimi.it>
2021-04-28 15:25:08 +0200

    Changes by paolo

--- src/main/java/it/polimi/ingsw/VaticanReport.java
+++ src/main/java/it/polimi/ingsw/VaticanReport.java
@@ -4,13 +4,33 @@ public class VaticanReport {
 
     private  int numberOfReports;
 
-    private final int[] reportsLength = new int[numberOfReports];
+    private int[] reportsLength = new int[numberOfReports];
 
-    private final int[] activationPosition = new int[numberOfReports];
+    private int[] activationPosition = new int[numberOfReports];
 
-    private final int[] popeFavourTile = new int[numberOfReports];
+    private int[] popeFavourTile = new int[numberOfReports];
 
-    private final boolean[] activatedEvent = new boolean[numberOfReports];
+    private boolean[] activatedEvent = new boolean[numberOfReports];
+
+    public void setNumberOfReports(int numberOfReports){
+        this.numberOfReports = numberOfReports;
+    }
+
+    public void setActivationPosition(int[] activationPosition){
+        this.activationPosition = activationPosition;
+    }
+
+    public void setPopeFavourTile(int[] popeFavourTile){
+        this.popeFavourTile = popeFavourTile;
+    }
+
+    public void setActivatedEvent(boolean[] activatedEvent){
+        this.activatedEvent = activatedEvent;
+    }
+
+    public void setReportsLength(int[] reportsLength) {
+        this.reportsLength = reportsLength;
+    }
 
     public int[] getReportsLength() {
         return reportsLength;

0987e39d3d86e8288267bca153500d9e042bcfd4
Samuele <10621029@polimi.it>
2021-05-30 17:37:25 +0200

    changes with code with me cli and tests

--- src/main/java/it/polimi/ingsw/cli/ActiveLeaderCardsDraw.java
+++ src/main/java/it/polimi/ingsw/cli/ActiveLeaderCardsDraw.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.cli;
 
+import it.polimi.ingsw.PersonalBoard;
+import it.polimi.ingsw.Player;
+import it.polimi.ingsw.leader.LeaderCard;
+
 public class ActiveLeaderCardsDraw {
+    public static void drawActiveLeaderCards(Player player ){
+        LeaderCard[] cardsArray = new LeaderCard[3];
+        cardsArray=player.getActiveLeaderCards();
+    }
 }
--- src/main/java/it/polimi/ingsw/cli/StrongboxDraw.java
+++ src/main/java/it/polimi/ingsw/cli/StrongboxDraw.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.cli;
 
+import it.polimi.ingsw.PersonalBoard;
 import it.polimi.ingsw.resources.Resource;
 import it.polimi.ingsw.resources.Strongbox;
 
@@ -9,35 +10,59 @@ public class StrongboxDraw {
 
     //private Strongbox strongboxDrawn;
 
-    public void drawStrongbox(Strongbox strongboxDrawn){
+    public static void drawStrongbox(PersonalBoard personalBoard){
         String resetEscape = Color.ANSI_RESET.escape();
-        int strongboxCoins = strongboxDrawn.getCoins();
-        int strongboxServants = strongboxDrawn.getServants();
-        int strongboxStones = strongboxDrawn.getStones();
-        int strongboxShields = strongboxDrawn.getShields();
-        int counter = 15;
-        int numberOfRows = (strongboxCoins+strongboxServants+strongboxShields+strongboxStones)%15;
-        while (numberOfRows != 0){
-            System.out.print(resetEscape + "-------------------------------------------------------------");
+        int resCounter = 0;
+        int numberOfRows[] = personalBoard.getStrongbox().getStrongboxResourcesAmount();
+        int numberOfRes = numberOfRows[0]+numberOfRows[1]+numberOfRows[2]+numberOfRows[3];
+        int numberOfRow = numberOfRes/15;
+        numberOfRow++;
+        System.out.print(resetEscape + "-------------------------------------------------------------");
+        while (numberOfRow != 0){
+            int counter = 15;
             System.out.print("\n");
-            while(strongboxCoins!=0&&counter!=0){
-                System.out.print(resetEscape + "| ");
-                System.out.print(Color.ANSI_YELLOW.escape() + "C " + resetEscape);
-                strongboxCoins--;
-                counter--;
+            while (counter!=0) {
+                if (resCounter<numberOfRes)
+                 {
+                 System.out.print(resetEscape + "| ");
+                 drawResource(personalBoard, resCounter);
+                  counter--;
+                  resCounter++;
+                 }
+                if(resCounter>=numberOfRes){
+                    System.out.print(resetEscape + "|   ");
+                    counter--;
+                }
+            }
+            System.out.print(resetEscape + "|");
+            System.out.print("\n");
+            System.out.print(resetEscape + "-------------------------------------------------------------");
+            numberOfRow--;
             }
         }
-    }
 
-    public void drawResource(int coins, int servants, int stones, int shields){
-        if(coins==0)
-            if(servants==0)
-                if(stones==0)
-                    if(shields==0){
-                        System.out.print(" ");
-                    }
-                else
-                    System.out.print(Color.ANSI_BLUE.escape() + "H ");
+
+    public static void drawResource(PersonalBoard personalBoards, int pos){
+        String resetEscape = Color.ANSI_RESET.escape();
+        Strongbox thisStrongbox = personalBoards.getStrongbox();
+        int k=0;
+        ArrayList<Resource> resources = new ArrayList<>();
+        for(int i : thisStrongbox.getStrongboxResourcesAmount()) {
+            for (int j = 0; j<i; j++) {
+                resources.add(new Resource(k));
+            }
+            k++;
+        }
+        if (resources.get(pos).equals(new Resource("coin")))
+            System.out.print(Color.ANSI_YELLOW.escape() + "C " + resetEscape);
+        if (resources.get(pos).equals(new Resource("stone")))
+            System.out.print(Color.ANSI_GREY.escape() + "R " + resetEscape);
+        if (resources.get(pos).equals(new Resource("shield")))
+            System.out.print(Color.ANSI_BLUE.escape() + "H " + resetEscape);
+        if (resources.get(pos).equals(new Resource("servant")))
+            System.out.print(Color.ANSI_PURPLE.escape() + "S " + resetEscape);
+        if (resources.get(pos).equals(null))
+            System.out.print(Color.ANSI_PURPLE.escape() + "  " + resetEscape);
 
     }
 

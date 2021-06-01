3ffe2f35e2bd9a69a353410945c5925c35d189fa
Samuele <10621029@polimi.it>
2021-06-01 17:54:48 +0200

    cli

--- src/main/java/it/polimi/ingsw/cli/ActiveLeaderCardsDraw.java
+++ src/main/java/it/polimi/ingsw/cli/ActiveLeaderCardsDraw.java
@@ -8,24 +8,54 @@ import it.polimi.ingsw.resources.Resource;
 public class ActiveLeaderCardsDraw {
     public static void drawActiveLeaderCards(Player player ){
         String resetEscape = Color.ANSI_RESET.escape();
-        LeaderCard[] cardsArray = new LeaderCard[3];
+        LeaderCard[] cardsArray = new LeaderCard[2];
         cardsArray=player.getActiveLeaderCards();
-        System.out.print(resetEscape + "--------------------------------------------");
-        System.out.print(resetEscape + "|Level: 3    |WP: 2       |Ability: CON");
+        System.out.print(resetEscape + "FIRST CARD");
+        System.out.print("\n");
+        System.out.print(resetEscape + "Required Resources: ");
+        drawReqRes(player, 0);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Required Cards: ");
+        drawReqCards(player, 0);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Ability: ");
+        abilityPrinter(player, 0);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Leader Resource: ");
+        drawRes(player, 0);
+        System.out.print("\n");
+        int WP = player.getActiveLeaderCards()[0].getWp();
+        System.out.print(resetEscape + "Win Points: " + WP);
+        System.out.print("\n");
+        System.out.print(resetEscape + "SECOND CARD");
+        System.out.print("\n");
+        System.out.print(resetEscape + "Required Resources: ");
+        drawReqRes(player, 1);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Required Cards: ");
+        drawReqCards(player, 1);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Ability: ");
+        abilityPrinter(player, 1);
+        System.out.print("\n");
+        System.out.print(resetEscape + "Leader Resource: ");
+        drawRes(player, 1);
+        System.out.print("\n");
+        int WP2 = player.getActiveLeaderCards()[1].getWp();
+        System.out.print(resetEscape + "Win Points: " + WP2);
+        System.out.print("\n");
     }
 
     public static void abilityPrinter(Player player, int index){
         LeaderCard[] arrayCards = player.getActiveLeaderCards();
         switch (arrayCards[index].getAbility()){
-            case 0:
-                Resource disc= ((LeaderOfDiscounts)arrayCards[index]).getDiscountedRes();
-                System.out.print(Color.ANSI_BLUE.escape() + "DIS");
+            case 0: System.out.print(Color.ANSI_BLUE.escape() + "Leader of discounts");
                 break;
-            case 1: System.out.print(Color.ANSI_BLUE.escape() + "DEP");
+            case 1: System.out.print(Color.ANSI_BLUE.escape() + "Leader of depots");
                 break;
-            case 2: System.out.print(Color.ANSI_BLUE.escape() + "CON");
+            case 2: System.out.print(Color.ANSI_BLUE.escape() + "Leader of conversions");
                 break;
-            case 3: System.out.print(Color.ANSI_BLUE.escape() + "PRO");
+            case 3: System.out.print(Color.ANSI_BLUE.escape() + "Leader of productions");
                 break;
         }
 
@@ -51,6 +81,73 @@ public class ActiveLeaderCardsDraw {
                 Resource prod = ((LeaderOfProductions) arrayCards[index]).getRequiredResource();
                 switchCases(resetEscape, prod);
             }
+        }
+    }
+
+    private static void switchCases(String resetEscape, Resource disc) {
+        int resIndex = disc.getResType();
+        switch (resIndex) {
+            case 0:
+                System.out.print(Color.ANSI_YELLOW.escape() + "C" + resetEscape);
+                break;
+            case 1:
+                System.out.print(Color.ANSI_GREY.escape() + "R" + resetEscape);
+                break;
+            case 2:
+                System.out.print(Color.ANSI_PURPLE.escape() + "S" + resetEscape);
+                break;
+            case 3:
+                System.out.print(Color.ANSI_BLUE.escape() + "H" + resetEscape);
+                break;
+        }
+    }
+
+
+    public static void drawReqRes(Player player, int index){
+        String resetEscape = Color.ANSI_RESET.escape();
+        Resource reqRes[] = player.getActiveLeaderCards()[index].getRequiredRes();
+        int reqRess[] = new int[10];
+        int coins = 0;
+        int stones = 0;
+        int servants = 0;
+        int shields = 0;
+        int j=0;
+        for (Resource i : reqRes){
+            reqRess[j]=i.getResType();
+            j++;
+        }
+        for (int l=0; l<10; l++){
+            switch (reqRess[l]){
+                case 0: coins++;
+                break;
+                case 1: stones++;
+                break;
+                case 2: servants++;
+                break;
+                case 3: shields++;
+                break;
+            }
+        }
+        if (coins!=0){
+            System.out.print(resetEscape + coins);
+            System.out.print(Color.ANSI_YELLOW.escape() + "C" + resetEscape);}
+        if (stones!=0){
+            System.out.print(resetEscape + stones);
+            System.out.print(Color.ANSI_GREY.escape() + "R" + resetEscape);}
+        if (servants!=0){
+            System.out.print(resetEscape + servants);
+            System.out.print(Color.ANSI_PURPLE.escape() + "S" + resetEscape);}
+        if (shields!=0){
+            System.out.print(resetEscape + shields);
+            System.out.print(Color.ANSI_BLUE.escape() + "H" + resetEscape);}
+    }
+
+    public static void drawReqCards(Player player, int index){
+        String resetEscape = Color.ANSI_RESET.escape();
+        int reqType[] = player.getActiveLeaderCards()[index].getRequiredType();
+        int reqLevel[] = player.getActiveLeaderCards()[index].getRequiredLevel();
+        for (int i: reqType){
+            System.out.print(Color.ANSI_CYAN.escape() + "t" + reqType[i] + "l" + reqLevel[i] + ", " + resetEscape);
         }
     }
 }
--- src/main/java/it/polimi/ingsw/cli/ProdCardsMarketDraw.java
+++ src/main/java/it/polimi/ingsw/cli/ProdCardsMarketDraw.java
@@ -1,4 +1,99 @@
 package it.polimi.ingsw.cli;
 
+import it.polimi.ingsw.Game;
+import it.polimi.ingsw.production.ProductionCard;
+import it.polimi.ingsw.resources.Resource;
+
 public class ProdCardsMarketDraw {
+    public static void drawProdCardsMarket(Game game){
+        ProductionCard[][] prodCardTensor= game.getCardsMarket().getTopCards();
+        String resetEscape = Color.ANSI_RESET.escape();
+        //System.out.print(resetEscape + "-------------------------------------------------------------------");
+        //System.out.print(resetEscape + "|Level: 3      WP: 3  |                     |                     |");
+        //System.out.print(resetEscape + "|Type: 2              |                     |                     |");
+        //System.out.print(resetEscape + "|Cost: A1 B1 C1 D1    |                     |                     |");
+        //System.out.print(resetEscape + "|Req Res: 4C 3B 2H 1B |                     |                     |");
+        //System.out.print(resetEscape + "|Given:               |                     |                     |");
+        //System.out.print(resetEscape + "|Given FP: 3          |                     |                     |");
+        System.out.print(resetEscape + "-------------------------------------------------------------------");
+        System.out.print("\n");
+        for (int i=0; i<4; i++) {
+            System.out.print(resetEscape + "|Level: " + prodCardTensor[0][i].getLevel() + "      WP: " + prodCardTensor[0][i].getWp() + "  ");
+            System.out.print(resetEscape + "|Level: " + prodCardTensor[1][i].getLevel() + "      WP: " + prodCardTensor[1][i].getWp() + "  ");
+            System.out.print(resetEscape + "|Level: " + prodCardTensor[2][i].getLevel() + "      WP: " + prodCardTensor[2][i].getWp() + "  |");
+            System.out.print("\n");
+
+            System.out.print(resetEscape + "|Type: " + prodCardTensor[0][i].getType() + "              ");
+            System.out.print(resetEscape + "|Type: " + prodCardTensor[1][i].getType() + "              ");
+            System.out.print(resetEscape + "|Type: " + prodCardTensor[2][i].getType() + "              |");
+            System.out.print("\n");
+
+            System.out.print(resetEscape + "!Cost: ");
+            drawResCounter(prodCardTensor[0][i].getCostArray());
+            System.out.print("    ");
+            System.out.print(resetEscape + "!Cost: ");
+            drawResCounter(prodCardTensor[1][i].getCostArray());
+            System.out.print("    ");
+            System.out.print(resetEscape + "!Cost: ");
+            drawResCounter(prodCardTensor[2][i].getCostArray());
+            System.out.print("    |");
+            System.out.print("\n");
+
+            System.out.print(resetEscape + "|Req Res: ");
+            drawResCounter(prodCardTensor[0][i].getRequiredRes());
+            System.out.print(" ");
+            System.out.print(resetEscape + "|Req Res: ");
+            drawResCounter(prodCardTensor[1][i].getRequiredRes());
+            System.out.print(" ");
+            System.out.print(resetEscape + "|Req Res: ");
+            drawResCounter(prodCardTensor[2][i].getRequiredRes());
+            System.out.print(" |");
+            System.out.print("\n");
+
+            System.out.print(resetEscape + "|Given: ");
+            drawResCounter(prodCardTensor[0][i].getGivenRes());
+            System.out.print("   ");
+            System.out.print(resetEscape + "|Given: ");
+            drawResCounter(prodCardTensor[1][i].getGivenRes());
+            System.out.print("   ");
+            System.out.print(resetEscape + "|Given: ");
+            drawResCounter(prodCardTensor[2][i].getGivenRes());
+            System.out.print("   |");
+            System.out.print("\n");
+
+            System.out.print(resetEscape + "|Given FP: " + prodCardTensor[0][i].getGivenFaithPoints() + "          ");
+            System.out.print(resetEscape + "|Given FP: " + prodCardTensor[1][i].getGivenFaithPoints() + "          ");
+            System.out.print(resetEscape + "|Given FP: " + prodCardTensor[2][i].getGivenFaithPoints() + "          |");
+            System.out.print("\n");
+            System.out.print(resetEscape + "-------------------------------------------------------------------");
+            System.out.print("\n");
+        }
+    }
+
+
+
+
+    private static void drawResCounter(Resource[] resourceArray) {
+        String resetEscape = Color.ANSI_RESET.escape();
+        int coins=0;
+        int stones=0;
+        int servants=0;
+        int shields=0;
+        for (int i=0; i<resourceArray.length; i++){
+            switch (resourceArray[i].toString()){
+                case "coin": coins++;
+                break;
+                case "stone": stones++;
+                break;
+                case "servant": servants++;
+                break;
+                case "shield": shields++;
+                break;
+            }
+        }
+        System.out.print(Color.ANSI_YELLOW.escape() + coins + "C " + resetEscape);
+        System.out.print(Color.ANSI_GREY.escape() + stones + "R " + resetEscape);
+        System.out.print(Color.ANSI_PURPLE.escape() + servants + "S " + resetEscape);
+        System.out.print(Color.ANSI_BLUE.escape() + shields + "H " + resetEscape);
+    }
 }
